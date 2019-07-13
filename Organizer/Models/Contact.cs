743d18172851b61f4f7856285cdd856f08e087b2
diman3003 <diman3003@mail.ru>
2019-07-13 09:40:54 +0400

    Initialization

---
+++ Organizer/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organizer.Models
+{
+    public class Contact
+    {
+        int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Company { get; set; }
+        public string Job { get; set; }
+
+    }
+}
